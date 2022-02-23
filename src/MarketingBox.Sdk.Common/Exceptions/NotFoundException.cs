027f37a6ae674b1d0f01e526681d2598dbfe0bc7
Lev Kurbanov <lev.k@smplt.net>
2022-02-23 21:15:39 +0300

    default message for not found exception

--- src/MarketingBox.Sdk.Common/Exceptions/NotFoundException.cs
+++ src/MarketingBox.Sdk.Common/Exceptions/NotFoundException.cs
@@ -4,6 +4,7 @@ namespace MarketingBox.Sdk.Common.Exceptions;
 
 public class NotFoundException : Exception
 {
+    public const string DefaultMessage = "There are no results for such request.";
     private const string ExceptionFormat = "{0}:{1} was not found.";
     public NotFoundException(string entityName, object value): base(string.Format(ExceptionFormat, entityName, value))
     { }
